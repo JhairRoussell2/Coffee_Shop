925c926a4f5b75ad421f7d5a808982d59ae17eff
JhairRoussell2 <jhairrmb3@gmail.com>
2025-03-31 03:58:49 -0500

    Detalles de un producto

--- coffee_shop/products/urls.py
+++ coffee_shop/products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductFormView, ProductListView
+from .views import ProductDetailView, ProductFormView, ProductListView, HomeView
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name="list_product"),
+    path('', HomeView.as_view(), name='home'),  # Ruta para la página de inicio
+    path('productos/', ProductListView.as_view(), name="list_product"),
     path('agregar/', ProductFormView.as_view(), name="add_product"),
+    path('producto/<int:pk>/', ProductDetailView.as_view(), name="product_detail"),  # Detalles de un producto
 ]
--- coffee_shop/products/views.py
+++ coffee_shop/products/views.py
@@ -2,6 +2,11 @@ from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ProductForm
 from .models import Product
+from django.views.generic import TemplateView
+from django.views.generic import DetailView
+
+
+
 
 
 # Create your views here.
@@ -18,3 +23,13 @@ class ProductListView(generic.ListView):
     model = Product
     template_name = "products/list_product.html"
     context_object_name = "products"
+
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = "home.html"  # Especificamos la plantilla para esta vista
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"  # La plantilla que se utilizará para mostrar el detalle
+    context_object_name = "product"  # El nombre del objeto que estará disponible en el contexto
